1e9fb4b2d53d59484229582d620229eaf10ddc14
Doracool <751379858@qq.com>
2015-12-02 20:18:10 +0800

    添加了刷新列表的事件，通过点击更改标题并在点击结束刷新界面

--- 01- UITableView/01- UITableView/ViewController.m
+++ 01- UITableView/01- UITableView/ViewController.m
@@ -8,14 +8,16 @@
 
 #import "ViewController.h"
 #import "QYHero.h"
-@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate,UIAlertViewDelegate>
 @property (nonatomic,strong) NSArray *heros;
+@property (nonatomic,strong) UITableView *tableView;
 @end
 
 @implementation ViewController
 //声明一个重用的ID
 static NSString *identifier = @"hero_cell";
 
+#pragma mark -懒加载
 - (NSArray *)heros
 {
     if (_heros == nil) {
@@ -33,6 +35,7 @@ static NSString *identifier = @"hero_cell";
     return _heros;
 }
 
+#pragma mark -数据源方法
 //每组的有多少行
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
@@ -53,9 +56,7 @@ static NSString *identifier = @"hero_cell";
     //**如果有则直接取出进行使用
     //**如果没有就在创建一个新的单元格
     
-    
 
-    
     //根据这个ID去缓存池中查找对应的cell
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
     
@@ -69,19 +70,41 @@ static NSString *identifier = @"hero_cell";
     cell.textLabel.text = model.name;
     cell.detailTextLabel.text = model.desc;
     
-    cell.accessoryType = UITableViewCellAccessoryDetailButton;
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     
     UIView *bgView = [[UIView alloc]init];
-    bgView.backgroundColor = [UIColor yellowColor];
+    bgView.backgroundColor = [UIColor lightGrayColor];
     cell.selectedBackgroundView = bgView;
-    
-    
-    //输出当前单元格的行索引
-    NSLog(@"%p----行索引:%ld",cell,indexPath.row);
+
     //返回单元格
     return cell;
     
     
+}
+
+#pragma mark -点击确定取消的响应事件
+//点击确定或取消的事件
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 1) {
+        //表示点击了确定
+        //更新数据
+        //1、获取用户文本框中的内容
+        NSString *name = [alertView textFieldAtIndex:0].text;
+        //2、找到对应的英雄模型
+        QYHero *hero = self.heros[alertView.tag];
+        
+        //3、修改对应英雄的模型
+        hero.name = name;
+        //4、刷新界面
+        //4.1、全部刷新
+        //[_tableView reloadData];
+        
+        //刷新单个行
+        NSIndexPath *idxPath = [NSIndexPath indexPathForRow:alertView.tag inSection:0];
+        [_tableView reloadRowsAtIndexPaths:@[idxPath] withRowAnimation:UITableViewRowAnimationTop];
+        
+    }
 }
 
 #pragma mark -代理方法
@@ -92,14 +115,28 @@ static NSString *identifier = @"hero_cell";
     //将点击的单元格字体变色
     cell.textLabel.textColor = [UIColor yellowColor];
     //设置右边栏的形式
-    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+//    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    //创建一个弹出框对象
+    UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"英雄名称" message:nil delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
+    
+    //获取当前被选中的这个英雄的名称
+    QYHero *hero = self.heros[indexPath.row];
+    alertView.tag = indexPath.row;
+    
+    //修改AlertView的样式显示出来一个文本框
+    alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+    
+    //获取那个文本框，并设置文本框中的文字为herosname
+    [alertView textFieldAtIndex:0].text = hero.name;
+   
+    [alertView show];
 }
 
 -(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    cell.textLabel.textColor = [UIColor redColor];
-    cell.accessoryType = UITableViewCellAccessoryNone;
+    cell.textLabel.textColor = [UIColor blackColor];
+//    cell.accessoryType = UITableViewCellAccessoryNone;
 }
 
 #pragma mark - 隐藏状态栏
@@ -110,16 +147,16 @@ static NSString *identifier = @"hero_cell";
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UITableView *tableView = [[UITableView alloc]initWithFrame:self.view.frame style:UITableViewStylePlain];
-    [self.view addSubview:tableView];
+    _tableView = [[UITableView alloc]initWithFrame:self.view.frame style:UITableViewStylePlain];
+    [self.view addSubview:_tableView];
     
     //设置数据源
-    tableView.dataSource = self;
-    
+    _tableView.dataSource = self;
+    _tableView.delegate = self;
     //设置行高
-    tableView.rowHeight = 70;
+    _tableView.rowHeight = 70;
     
-    tableView.allowsSelection = YES;
+    _tableView.allowsSelection = YES;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
